57fc0d80a3b8a9eae0b398430ff3a76257138093
deetriplett <deetriplett@gmail.com>
2017-01-14 10:28:23 -0500

    Added console log

--- scripts.js
+++ scripts.js
@@ -3,3 +3,5 @@ alert("Hello from Sugar Fox");
 document.write("<h1>Welcome!");
 document.write("<h2>From Our Family to Yours</h2>");
 document.write("<h3>Happy Holidays!</h3>");
+
+console.log("Program finished!");
