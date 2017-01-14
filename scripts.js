925b10dae0d4fac56b283af3b20fbc2923129986
deetriplett <deetriplett@gmail.com>
2017-01-14 10:42:14 -0500

    Added new message

--- scripts.js
+++ scripts.js
@@ -2,6 +2,10 @@ var message = "Hello From Sugar Fox!";
 
 alert(message);
 
+message = "We are glad you're here.";
+
+alert(message);
+
 document.write("<h1>Welcome!");
 document.write("<h2>From Our Family to Yours</h2>");
 document.write("<h3>Happy Holidays!</h3>");
