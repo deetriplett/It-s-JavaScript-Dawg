ff030a6d6b4e92f3ff152144a33b269e0a0672be
deetriplett <deetriplett@gmail.com>
2017-01-14 11:26:54 -0500

    added var str
    Exploring uppercase, lowercase, and string length in the console

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ var message = "Hello! I'm Sugar Fox!";
 
 alert(message);
 
+//Ask a question and capture response prompt();
 var visitorName = prompt("What is your name?");
 
 message = "We are glad you're here " + visitorName;
@@ -11,6 +12,17 @@ alert(message);
 document.write("<h1>Welcome!");
 document.write("<h2>From Our Family to Yours</h2>");
 
-//Ask a question and capture response prompt();
+// opens an alert dialog with the number 9
+var str = '9 letters';
+console.log( str.length ); 
+
+// opens an alert dialog with the string 'UPPER'
+var str = 'upper';
+console.log( str.toUpperCase() ); 
+
+// opens an alert dialog with the string 'lower'
+var str = 'LOWER';
+console.log( str.toLowerCase() ); 
+
 
 console.log("Program finished!");
