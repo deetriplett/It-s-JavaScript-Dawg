f736ab2a860120408bceb4c382ecbf20623fb3f3
deetriplett <deetriplett@gmail.com>
2017-01-14 10:18:52 -0500

    Edited display wording

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
-alert("Hello from Dawn!");
-document.write("<h1>Welcome to your Basic Javascript!</h1>");
-alert("Thanks for visiting!");
+alert("Hello from Sugar Fox");
+
+document.write("<h1>Welcome!");
+document.write("<h2>From Our Family to Yours</h2>");
+document.write("<h3>Happy Holidays!</h3>");
