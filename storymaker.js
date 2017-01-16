026d5615f836441397fbc4c535d8c5e9658d49cc
deetriplett <deetriplett@gmail.com>
2017-01-16 18:28:38 -0500

    created var sentence
    combined all doc.writes into one variable, then printed var to doc

--- storymaker.js
+++ storymaker.js
@@ -11,10 +11,12 @@ var verb = prompt("Verb");
 
 alert("You are done! Now let's see that story!");
 
-document.write("<h2>" + name + " is our pet " + animal + ". She was left home alone. </h2>");
-document.write("<h2>" + name + " " + action + " on the " + furniture + " and ate the bread. </h2>");
-document.write("<h2>" + exclaim.toUpperCase() + "! Mom yelled. </h2>"); 
-document.write("<h2> Now we have to make sandwiches out of " + food + ". They tasted " + verb + ". </h2>");
+var sentence = ("<h2>" + name + " is our pet " + animal + ". She was left home alone. </h2>");
+sentence += ("<h2>" + name + " " + action + " on the " + furniture + " and ate the bread. </h2>");
+sentence += ("<h2>" + exclaim.toUpperCase() + "! Mom yelled. </h2>"); 
+sentence += ("<h2> Now we have to make sandwiches out of " + food + ". They tasted " + verb + ". </h2>");
+
+document.write(sentence); 
 
 console.log(name);
 console.log(animal);
