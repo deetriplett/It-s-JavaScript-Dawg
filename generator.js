ff12b4011ff70b18d60fc3bf439c5c625013335c
deetriplett <deetriplett@gmail.com>
2017-01-16 20:05:54 -0500

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,12 +1,17 @@
-//RANDOM NUMBER GENERATOR
+//RANDOM NUMBER GENERATOR//
 
 //PROMPT TO COLLECT USER INFO
+var userInput = prompt("Let's create a random number together. Get the ball rolling with a number of your own: ");
 
 //PARSEINT() TO CONVERT INPUT TO NUMBER
+var userNum = parseInt(userInput); 
 
 //MATH.RANDOM() METHOD TO CREATE RANDOM NUMBER
+var randomNum = Math.random(userNum);
 
 //PRINT BACK TO DOC
+var answer = "Your random number is " + randomNum + ".";
 
+document.write(answer);
 
 
