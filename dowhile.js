ff45136544506830b01ab52f1d4b99621fefc67b
deetriplett <deetriplett@gmail.com>
2017-01-19 16:44:51 -0500

    Update dowhile.js
    Guess Random number game

--- dowhile.js
+++ dowhile.js
@@ -1,1 +1,15 @@
-// Do While Loops
+// Do While Loops, DOESNT CHECK CONDITION UNTIL IT RUNS ONCE
+
+// do {
+//  } while ()
+           
+//Guess the Number Game
+
+var randomNumber = getRandomNumber(10);
+var guess;
+var guessCount = 0;
+
+function getRandomNumber (upper) {
+  var num = Math.floor(Math.random() * upper) + 1;
+  return num;
+}
