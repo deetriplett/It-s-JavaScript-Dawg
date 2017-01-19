7515a8a1f8ca4ebb2930a161e6ef7ac8bf658764
deetriplett <deetriplett@gmail.com>
2017-01-19 16:53:39 -0500

    Update dowhile.js
    added guess prompt to do..while loop

--- dowhile.js
+++ dowhile.js
@@ -10,6 +10,15 @@ var guess;
 var guessCount = 0;
 
 function getRandomNumber (upper) {
-  var num = Math.floor(Math.random() * upper) + 1;
+  var num = Math.floor(Math.random() * upper) +1;
   return num;
 }
+
+do {
+  guess = prompt("I am thinking of a number between 1 and 10. What is it?");
+  guessCount += 1;
+  if (parseint(guess) === randomNumber) {
+  
+  }
+           
+} while ( )
