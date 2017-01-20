73205ae59751617eac516ab78aa7baa16d190e64
Dawn Monroe <deetriplett@gmail.com>
2017-01-20 11:15:59 -0500

    Create objects.js

---
+++ js/objects.js
@@ -0,0 +1,23 @@
+//Objects have properties and methods:
+//  Properties = a variable that belongs to the object
+//  Methods = something the object can do or that can be done to the object
+//  {} creates an object, [] creates an array 
+//  Example: 
+//    var student = {
+//      name: "Dawn",
+//      grades: [80, 85, 90, 95]
+//     };
+//  
+//  
+//  
+//  
+//  
+
+var person = {
+  name: "Jackson",
+  age: 5,
+  country: "US",
+  student: true
+  };
+
+
