fb16390004a93ac248b29f0fab7748d8c3a6a1b1
deetriplett <deetriplett@gmail.com>
2017-01-19 20:26:10 -0500

    created new variables
    and empty question array

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,17 @@
+var questions = [
+  []
+  []
+  []
+  []
+   
+]
+
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+var html;
+
 
 function print(message) {
   document.write(message);
