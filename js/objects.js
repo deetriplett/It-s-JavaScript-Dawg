090687d5536e8c1866823a5c628fcd4ebd0013d5
Dawn Monroe <deetriplett@gmail.com>
2017-01-20 11:22:37 -0500

    create print function

--- js/objects.js
+++ js/objects.js
@@ -1,7 +1,7 @@
 //Objects have properties and methods:
 //  Properties = a variable that belongs to the object
 //  Methods = something the object can do or that can be done to the object
-//  {} creates an object, [] creates an array 
+//  {} creates an object (uses keys to access data), [] creates an array (uses index numbers to access values)
 //  Example: 
 //    var student = {
 //      name: "Dawn",
@@ -14,10 +14,16 @@
 //  
 
 var person = {
-  name: "Jackson",
-  age: 5,
+  name: "Dawn",
+  age: 28,
   country: "US",
-  student: true
+  student: true,
+  skills: ['JavaScript','HTML','CSS']
   };
+
+function print(message) {
+  var div = document.getElementById('output');
+  div.innerHTML = message;
+}
 
 
