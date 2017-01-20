0f073b02e608027646a826de2e250ed221ed150e
deetriplett <deetriplett@gmail.com>
2017-01-19 20:29:16 -0500

    Added array

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,9 @@
 var questions = [
-  []
-  []
-  []
-  []
-   
-]
+  ["How many states are in the United States?", 50]
+  ["How many continents are there?", 7]
+  ["How many legs does an insect have", 6]
+  ["How many side are there of an octagon?", 8] 
+];
 
 var correctAnswers = 0;
 var question;
