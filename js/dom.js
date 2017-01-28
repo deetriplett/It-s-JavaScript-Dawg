2c89b19ee3814ff9f317b416112c66ac5f7f0edd
Dawn Monroe <deetriplett@gmail.com>
2017-01-28 17:52:04 -0500

    add mouse over event, comments

--- js/dom.js
+++ js/dom.js
@@ -1,43 +1,55 @@
+//Const Variables//
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
-const descriptionInput =document.querySelector('input.description');
+const descriptionInput = document.querySelector('input.description');
 const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
+const listItems = document.getElementsByTagName('li');
 
+//Mouse Over Event for List//
+for (let index = 0; index < listItems.length; index +=1) {
+  listItems[index].addEventListener('mouseover', () => {
+    listItems[index].textContent = listItems[index].textContent.toUpperCase();
+  });
+  listItems[index].addEventListener('mouseout', () => {
+    listItems[index].textContent = listItems[index].textContent.toLowerCase();
+  });
+}
+
+//Hide-Show Toggle for List//  
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Hide List';
+    listDiv.style.display = 'block';
+  } else {
+  toggleList.textContent = 'Show List';
+  listDiv.style.display = 'none';
+  }  
+});
+
+//Change List Description//
 descriptionButton.addEventListener('click', () => {
   descriptionP.innerHTML = descriptionInput.value + ': ';
   descriptionInput.value = '';
 });
   
-p.title = "List Description";
-  
-toggleList.addEventListener('click', () => {
-  if (listDiv.style.display == "none") {
-    toggleList.textContent = "Hide List";
-    listDiv.style.display = 'block';
-  } else {
-  toggleList.textContent = "Show List";
-  listDiv.style.display = 'none';
-  }
-  
-});
-  
+//Add New List Item//
 addItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
-  li.textContext.addItemInput.value;
+  li.textContent.addItemInput.value;
   ul.appendChild(li);
   addItemInput.value = '';
 });
-  
-  removeItemButton.addEventListener('click', () => {
+
+//Remove List Item//
+removeItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.querySelector('li:last-child');
   ul.removeChild(li);
- 
 });
 
   
